5919165bc041e232bcca3430321e77b1b93b1b07
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-02-12 15:21:47 -0300

    first commit

---
+++ fundamentos/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello World!");
+//declarando variaveis
+byte valor1 = 255;
+Console.WriteLine(valor1);
+sbyte valor2 = -128;
+Console.WriteLine(valor2);
+int valor3 = -2147483647;
+Console.WriteLine(valor3);
+uint valor4 = 4294967295;
+Console.WriteLine(valor4);
+short valor5 = -32768;
+Console.WriteLine(valor5);
+ushort valor6 = 65535;
+Console.WriteLine(valor6);
+
+const int valor7 = 100;
+Console.WriteLine(valor7);
+
+Console.ReadLine();
