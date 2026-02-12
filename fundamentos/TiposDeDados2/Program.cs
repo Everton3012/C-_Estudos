8bd04ffe6d16c66ffe29191a1a4048c83f0c15c2
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-02-12 15:47:51 -0300

    remover arquivos de configuração e projeto desnecessários

---
+++ fundamentos/TiposDeDados2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Atrobuindo valores a double, float e decimal");
+Console.WriteLine();
+//declarando variaveis
+double valor1 = 1.234;
+float valor2 = 1.234f;
+decimal valor3 = 1.234m;
+
+Console.WriteLine("Valor double: " + valor1);
+Console.WriteLine("Valor float: " + valor2);
+Console.WriteLine("Valor decimal: " + valor3);
+
+Console.ReadLine();
+
+float x = 1f / 3f;
+double y = 1d / 3d;
+decimal z = 1m / 3m;
+Console.WriteLine("Valor de x: " + x);
+Console.WriteLine("Valor de y: " + y);
+Console.WriteLine("Valor de z: " + z);
+
+Console.ReadLine();
