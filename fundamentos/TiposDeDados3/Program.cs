8f682ce8c1013db86c09b3552d9ecc10e96f1501
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-02-12 16:08:52 -0300

    adicionar projeto básico com exemplos de tipos de dados e remover .gitignore

---
+++ fundamentos/TiposDeDados3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Atrobuindo valores para boolean e char");
+Console.WriteLine();
+//declarando variaveis
+bool ativo = true;
+System.Boolean inativo = false;
+Boolean inativo2 = false;
+
+Console.WriteLine("Valor boolean: " + ativo);
+Console.WriteLine("Valor boolean: " + inativo);
+Console.WriteLine("Valor boolean: " + inativo2);
+
+Console.WriteLine(10 == 10);
+Console.WriteLine(10 == 5);
+
+Console.ReadLine();
+
+char letra1 = 'A';
+char letra2 = '\u0035';
+
+Console.WriteLine("Valor char: " + letra1);
+Console.WriteLine("Valor char: " + letra2);
