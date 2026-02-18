2d36bc3ae6b3383a7000c8177babce9a94f8bba2
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-02-17 23:17:19 -0300

    adicionar projeto de conversão de tipos com exemplos de conversão implícita e explícita

---
+++ fundamentos/ConversaoDeTipos/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace consoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversão de Tipos");
+
+            int varInt = 100;
+            double varDouble = varInt; // Conversão implícita de int para double
+            long numeroLong = (long) varDouble;// Conversão explícita de double para long
+            Console.WriteLine($"Valor do double: {numeroLong}");
+
+            int varInt2 = 200;
+            double varDouble2 = 100.500;
+
+            float resultado = (float) (varInt2 / varDouble2); // Conversão explícita de double para float
+
+            bool valorBool = true;
+           
+
+            System.Console.WriteLine($"Resultado da divisão: { resultado.ToString()}");
+
+            System.Console.WriteLine(Convert.ToString(valorBool));
+            System.Console.WriteLine(valorBool);
+            System.Console.WriteLine(valorBool.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
