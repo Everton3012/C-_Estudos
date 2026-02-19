4cbff7fdb6fcbeb1b95041c13758cccd3cacf4c5
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-02-19 17:59:16 -0300

    adicionar projeto de entrada de dados com exemplos de leitura e conversão de tipos

---
+++ fundamentos/EntradaDeDados/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace consoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrada de Dados");
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite sua altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Olá, {nome}!");
+            Console.WriteLine($"sua idade é de {idade} anos!");
+                Console.WriteLine($"sua altura é de {altura} metros!");
+            Console.ReadKey();
+        }
+    }
+}
